b379a40b93a59790ac023ed9d1af52acbd0b1af2
Ozodbek1-AI <ozodbeknetwork@gmail.com>
2025-10-18 19:46:40 +0500

    user comment all get list update

--- apps/pages/urls.py
+++ apps/pages/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from apps.pages.views import CreateUserCommentAPIView
+from apps.pages.views import CreateUserCommentAPIView, UserCommentListAPIView
 
 app_name = 'pages'
 
 urlpatterns = [
-    path('user-create-comment/',CreateUserCommentAPIView.as_view())
+    path('user-create-comment/',CreateUserCommentAPIView.as_view()),
+    path('user-detail-comment/',UserCommentListAPIView.as_view()),
 ]
--- apps/pages/views.py
+++ apps/pages/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from apps.pages.models import Tweet
 from apps.pages.serializer import CreateUserCommitSerializer
 
 
@@ -10,12 +12,27 @@ class CreateUserCommentAPIView(APIView):
     serializer_class = CreateUserCommitSerializer
     permission_classes = [IsAuthenticated]
 
+
     def post(self,request):
         serializer = self.serializer_class(data=request.data)
 
         if serializer.is_valid():
             serializer.save(user=request.user)
-            print(request.data)
-            print(serializer.data)
             return Response(data={"message":"Comment is created","comment":serializer.data},status=201)
         return Response(data={"message":"Comment does not created","errors":serializer.errors},status=404)
+
+class UserCommentListAPIView(APIView):
+    serializer_class = CreateUserCommitSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get(self,request):
+
+        user = request.user
+        comments = Tweet.objects.filter(user=user)
+
+        if not comments.exists():
+            return Response({"message": "User has no comments"}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = self.serializer_class(comments, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
